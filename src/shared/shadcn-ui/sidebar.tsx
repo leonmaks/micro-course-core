c2d3a45ab8835983f919d6ccc6d60226b8452a1d
leonmaks <le_ma_@mail.ru>
2025-09-14 17:20:32 +0300

    errors in shadcn-ui

--- src/shared/shadcn-ui/sidebar.tsx
+++ src/shared/shadcn-ui/sidebar.tsx
@@ -6,7 +6,7 @@ import { cva, VariantProps } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
 
 import { useIsMobile } from "@/shared/hooks/use-mobile"
-import { cn } from "@/shared/lib/utils"
+import { cn } from "@/shared/lib/shadcn-ui-utils"
 import { Button } from "@/shared/shadcn-ui/button"
 import { Input } from "@/shared/shadcn-ui/input"
 import { Separator } from "@/shared/shadcn-ui/separator"
@@ -569,7 +569,7 @@ function SidebarMenuAction({
         "peer-data-[size=lg]/menu-button:top-2.5",
         "group-data-[collapsible=icon]:hidden",
         showOnHover &&
-          "peer-data-[active=true]/menu-button:text-sidebar-accent-foreground group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 md:opacity-0",
+        "peer-data-[active=true]/menu-button:text-sidebar-accent-foreground group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 md:opacity-0",
         className
       )}
       {...props}
